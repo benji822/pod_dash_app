9d7c0ea535ff7508f361de748e2a018a0b7b782d
Benjamin <benng822@gmail.com>
2021-12-14 15:06:50 +0800

    change env to venv

--- app.py
+++ app.py
@@ -135,5 +135,5 @@ def update_graph(data_date, data_line, data_workcell):
     return fig, df_test.to_dict('records')
 
 if __name__ == '__main__':
-    # app.run_server(debug=True)
-    app.run_server(host='0.0.0.0', port=8050, debug=True)
+    app.run_server(debug=True)
+    # app.run_server(host='0.0.0.0', port=8050, debug=True)
